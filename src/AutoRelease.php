b6ac0f6c20d498795ddb7d7e2e3bb531dd2146e7
Sylvain Roh <syzin12@gmail.com>
2019-04-15 16:47:44 +0200

    Initial commit

--- src/AutoRelease.php
+++ src/AutoRelease.php
@@ -2,24 +2,41 @@
 namespace rohsyl\GithubAutoRelease;
 
 use http\Env\Request;
+use rohsyl\GithubAutoRelease\Utils\Config;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 class AutoRelease
 {
+    private $log;
+
+    public function __construct() {
+
+        Config::init();
+
+        $this->log = new Logger('github-auto-release');
+        $this->log->pushHandler(new StreamHandler(Config::get('LOG_PATH'), Logger::DEBUG));
+    }
+
     public function handle() {
         $request = new Request();
 
         $githubPayload = $request->getContent();
         $githubHash = $request->header('X-Hub-Signature');
 
-        $localToken = config('app.deploy_secret');
+        $localToken = Config::get('GITHUB_SECRET_TOKEN');
 
         $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
+        
+        $this->log->debug('Request get : \n');
+
         if (hash_equals($githubHash, $localHash)){
 
+            print_r($request->getBody());
+            print_r($githubPayload);
 
-
+            $this->log->debug($request->getBody());
         }
 
-        print_r($request->getBody());
     }
 }
